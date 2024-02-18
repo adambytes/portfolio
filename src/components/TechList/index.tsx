163b6070c1e55d8cba2da7f66142d07ebb6664ad
Adam Liang <hello@adamliang.dev>
2024-02-18 18:37:51 -0500

    feat: added remove and auto return functionality to icons
    Signed-off-by: Adam Liang <hello@adamliang.dev>

--- src/components/TechList/index.tsx
+++ src/components/TechList/index.tsx
@@ -1,22 +1,25 @@
 'use client'
 import AWS from '@/public/images/AWS.svg'
 import Azure from '@/public/images/Azure.svg'
-import Docker from '@/public/images/Docker.svg'
-import GCP from '@/public/images/Google Cloud.svg'
+import Bun from '@/public/images/Bun.svg'
 import Django from '@/public/images/Django.svg'
+import Docker from '@/public/images/Docker.svg'
 import GitHubActions from '@/public/images/GitHub Actions.svg'
+import GCP from '@/public/images/Google Cloud.svg'
 import Java from '@/public/images/Java.svg'
 import JavaScript from '@/public/images/JavaScript.svg'
 import Kubernetes from '@/public/images/Kubernetes.svg'
+import NextJS from '@/public/images/NextJs.svg'
 import NodeJs from '@/public/images/NodeJs.svg'
 import Python from '@/public/images/Python.svg'
 import React from '@/public/images/React.svg'
+import Redux from '@/public/images/Redux.svg'
 import TypeScript from '@/public/images/TypeScript.svg'
 import Webpack from '@/public/images/Webpack.svg'
-import NextJS from '@/public/images/NextJs.svg'
+
 import cn from '@/utils/cn'
-import { m, LazyMotion, domAnimation } from 'framer-motion'
-import { useMemo } from 'react'
+import { m, LazyMotion, domAnimation, AnimatePresence } from 'framer-motion'
+import { MouseEventHandler, useEffect, useState, useMemo } from 'react'
 
 const styles = {
   icon: 'w-8 h-8',
@@ -24,43 +27,64 @@ const styles = {
 }
 
 export default function TechList() {
-  const svgList = useMemo(
-    () => [
+  const svgList = useMemo<
+    Record<string, React.FC<Partial<SVGSVGElement>> | undefined>
+  >(() => {
+    return {
       AWS,
       Azure,
-      Docker,
-      GCP,
+      Bun,
       Django,
+      Docker,
       GitHubActions,
+      GCP,
       Java,
       JavaScript,
       Kubernetes,
+      NextJS,
       NodeJs,
       Python,
       React,
+      Redux,
       TypeScript,
       Webpack,
-      NextJS,
-    ],
-    []
-  )
+    }
+  }, [])
+
+  const [show, setShow] = useState<string[]>(Object.keys(svgList))
+
+  const handleIconClick: MouseEventHandler = (e) => {
+    console.log('clicked', e.currentTarget.id)
+    const name = e.currentTarget.id
+    // setDeleted((prev) => [...prev, name])
+    setShow((prev) => prev.filter((item) => item !== name))
+
+    Promise.resolve(
+      setTimeout(() => {
+        setShow((prev) => [...prev, name])
+      }, 2000)
+    )
+  }
 
   return (
     <LazyMotion features={domAnimation}>
       <m.div
-        className="grid grid-cols-5 gap-8 py-4"
+        className="grid grid-cols-5 gap-8 py-4 "
         variants={{
           hidden: { opacity: 0 },
           show: { opacity: 1, transition: { staggerChildren: 0.2 } },
-          exit: { opacity: 0 },
         }}
         initial="hidden"
         animate="show"
       >
-        {svgList.map((Icon: any, i: number) => {
+        {show.map((name: string, i: number) => {
+          const Icon = svgList[name]
+
           return (
             <m.div
-              key={'icon' + i}
+              key={`icon${i}`}
+              onClick={handleIconClick}
+              id={name}
               className={cn(
                 styles.iconContainer,
                 'shadow-lg shadow-slate-800 hover:scale-110 transition-transform duration-200 ease-in-out'
@@ -69,8 +93,11 @@ export default function TechList() {
                 hidden: { opacity: 0 },
                 show: { opacity: 1 },
               }}
+              layout
             >
-              <Icon alt="icon" className={styles.icon} width={32} height={32} />
+              {Icon && (
+                <Icon key={`svg${i}`} id={name} className={styles.icon} />
+              )}
             </m.div>
           )
         })}
